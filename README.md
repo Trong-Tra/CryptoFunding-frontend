2f4b2ca5149c149e3882c8e38f8865a4994c6a3d
Minh Trong <traminhtrong2604@gmail.com>
2023-11-13 12:32:47 +0700

    commit #1

---
+++ README.md
@@ -0,0 +1,50 @@
+# CryptoFunding
+
+<h3 align="center">CryptoFunding</h3>
+<p align="center">
+A basic crowd funding smart contract 
+</p>
+<hr>
+
+## About the projects
+
+-   Welcome to CryptoFunding, a basic crowd funding smart contract design to raise fund.
+-   With CryptoFunding, contributors will become a part of the contract network when they fund it.
+-   Withdrawing in CryptoFunding is only available to the owner of the contract.
+
+### Built with
+
+<div align="center">
+    <img src="https://skillicons.dev/icons?i=html,css,js"/> <br>
+</div>
+Note that this project built with ethers 6.7.1 so it might not working for future versions
+
+CryptoFunding is open-source licensed under the MIT License
+
+## Getting Started
+
+To get started with CryptoFunding, follow these steps:
+
+# Using the contract
+
+1. Go to the backend repository: "https://github.com/AgonyThorn/CryptoFunding.git"
+
+2. Deploy the Smart Contract: Deploy the CryptoFunding smart contract on your chosen Ethereum network.
+
+# Checking out the dApp
+
+1. Go to the backend repository: "https://github.com/AgonyThorn/CryptoFunding.git"
+
+2. Install node (using along with yarn if it comfortable with you)
+
+3. Deploy network for the contract in the backend
+
+4. Import one of the decoy account into your metamask (recommend using a metamask without any actual currency)
+
+5. Either clone this repo and run it yourself or click this:
+
+## Security Considerations
+
+-   Ensure that you deploy the contract on a secure Ethereum network.
+-   Review and test the contract thoroughly to identify and mitigate potential security risks.
+<hr>
---
+++ index.js
@@ -0,0 +1,98 @@
+import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+const withdrawButton = document.getElementById("withdrawButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+balanceButton.onclick = getBalance
+withdrawButton.onclick = withdraw
+
+function showMessage(message) {
+    let delay = 2000
+    document.getElementById("messageArea").innerHTML = message
+
+    setTimeout(() => {
+        document.getElementById("messageArea").innerHTML = ""
+    }, delay)
+}
+
+function showMessages(messages, index = 0, delay = 2000) {
+    if (index < messages.length) {
+        showMessage(messages[index])
+
+        setTimeout(() => {
+            showMessages(messages, index + 1, delay)
+        }, delay)
+    }
+}
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+        connectButton.innerHTML = "Connected!"
+        showMessages(["Connecting...", "Connected successfully 🦊"])
+    } else {
+        showMessage("Please install metamask 🦊")
+    }
+}
+
+async function getBalance() {
+    if (typeof window.ethereum != "undefined") {
+        const provider = new ethers.BrowserProvider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        console.log(`Contract current balance`, ethers.formatEther(balance))
+        showMessages([
+            "Confirming owner process...",
+            `The contract current balance is ${ethers.formatEther(balance)}`,
+        ])
+    }
+}
+
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.BrowserProvider(window.ethereum)
+        const signer = await provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.parseEther(ethAmount),
+            })
+            await listenForTransactionMine(transactionResponse, provider)
+            console.log("Done!")
+            showMessages(["Funding process initiated!", "Thank you 🦊"])
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`)
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            let confirmNum = transactionReceipt.confirmations()
+            console.log(`Completed with ${confirmNum} confirmations`)
+            resolve()
+        })
+    })
+}
+
+async function withdraw() {
+    if (typeof window.ethereum != "undefined") {
+        console.log("Withdrawing...")
+        const provider = new ethers.BrowserProvider(window.ethereum)
+        const signer = await provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
